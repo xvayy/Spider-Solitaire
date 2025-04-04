73671d19d711ba57cf50a97ca79aff122ee70aa2
Yuliia Ivaniuk <ivanykjula@gmail.com>
2025-04-04 07:38:27 +0300

    Add structure of project

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function init() {
+    import('./game.js');
+}
+
+const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener('htmx:afterOnLoad', () => {
+    loadedPartialsCount++;
+    if (loadedPartialsCount === totalPartials) init();
+});
+
+console.log("BEBE GAME");
